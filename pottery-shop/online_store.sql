fb7e72f99a47381028542e63ab676d2e79d8b2ff
tu <tu@gmail.com>
2021-10-26 10:57:40 +0700

    tuday

---
+++ pottery-shop/online_store.sql
@@ -0,0 +1,439 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 23, 2021 lúc 06:01 AM
+-- Phiên bản máy phục vụ: 10.4.18-MariaDB
+-- Phiên bản PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `online_store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `chi_muc_gio_hang`
+--
+
+CREATE TABLE `chi_muc_gio_hang` (
+  `id` bigint(20) NOT NULL,
+  `so_luong` int(11) NOT NULL,
+  `ma_gio_hang` bigint(20) DEFAULT NULL,
+  `ma_san_pham` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `chi_tiet_don_hang`
+--
+
+CREATE TABLE `chi_tiet_don_hang` (
+  `id` bigint(20) NOT NULL,
+  `don_gia` bigint(20) NOT NULL,
+  `so_luong` int(11) NOT NULL DEFAULT 1,
+  `ma_don_hang` bigint(20) DEFAULT NULL,
+  `ma_san_pham` bigint(20) DEFAULT NULL,
+  `so_luong_dat` int(11) NOT NULL,
+  `so_luong_nhan_hang` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `danh_muc`
+--
+
+CREATE TABLE `danh_muc` (
+  `id` bigint(20) NOT NULL,
+  `ten_danh_muc` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `danh_muc`
+--
+
+INSERT INTO `danh_muc` (`id`, `ten_danh_muc`) VALUES
+(1, 'BÌNH TÀI LỘC'),
+(2, 'BÌNH HOA - LỌ HOA'),
+(3, 'TRỨNG PHONG THỦY'),
+(4, 'HỒ LÔ GỐM SỨ'),
+(5, 'ĐÈN BÀN GỐM'),
+(6, 'TRANH GỐM'),
+(7, 'TƯỢNG GỐM'),
+(8, '\r\n\r\nĐĨA CẢNH ');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `don_hang`
+--
+
+CREATE TABLE `don_hang` (
+  `id` bigint(20) NOT NULL,
+  `dia_chi_nhan` varchar(255) DEFAULT NULL,
+  `ghi_chu` varchar(255) DEFAULT NULL,
+  `ho_ten_nguoi_nhan` varchar(255) DEFAULT NULL,
+  `ngay_dat_hang` datetime DEFAULT NULL,
+  `ngay_giao_hang` datetime DEFAULT NULL,
+  `ngay_nhan_hang` datetime DEFAULT NULL,
+  `sdt_nhan_hang` varchar(255) DEFAULT NULL,
+  `trang_thai_don_hang` varchar(255) DEFAULT NULL,
+  `ma_nguoi_dat` bigint(20) DEFAULT NULL,
+  `ma_shipper` bigint(20) DEFAULT NULL,
+  `tong_gia_tri` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `gio_hang`
+--
+
+CREATE TABLE `gio_hang` (
+  `id` bigint(20) NOT NULL,
+  `tong_tien` varchar(255) DEFAULT NULL,
+  `ma_nguoi_dung` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hang_san_xuat`
+--
+
+CREATE TABLE `hang_san_xuat` (
+  `id` bigint(20) NOT NULL,
+  `ten_hang_san_xuat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `hang_san_xuat`
+--
+
+INSERT INTO `hang_san_xuat` (`id`, `ten_hang_san_xuat`) VALUES
+(2, 'BÁT TRÀNG');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `lien_he`
+--
+
+CREATE TABLE `lien_he` (
+  `id` bigint(20) NOT NULL,
+  `email_lien_he` varchar(255) DEFAULT NULL,
+  `ngay_lien_he` datetime DEFAULT NULL,
+  `ngay_tra_loi` datetime DEFAULT NULL,
+  `noi_dung_lien_he` varchar(255) DEFAULT NULL,
+  `noi_dung_tra_loi` varchar(255) DEFAULT NULL,
+  `trang_thai` varchar(255) DEFAULT NULL,
+  `ma_nguoi_tra_loi` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nguoidung_vaitro`
+--
+
+CREATE TABLE `nguoidung_vaitro` (
+  `ma_nguoi_dung` bigint(20) NOT NULL,
+  `ma_vai_tro` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `nguoidung_vaitro`
+--
+
+INSERT INTO `nguoidung_vaitro` (`ma_nguoi_dung`, `ma_vai_tro`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nguoi_dung`
+--
+
+CREATE TABLE `nguoi_dung` (
+  `id` bigint(20) NOT NULL,
+  `dia_chi` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `ho_ten` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `so_dien_thoai` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `nguoi_dung`
+--
+
+INSERT INTO `nguoi_dung` (`id`, `dia_chi`, `email`, `ho_ten`, `password`, `so_dien_thoai`) VALUES
+(1, NULL, 'admin@gmail.com', 'Nguyễn Xuân Nam', '$2a$10$/VFMNUPBKNVRMjxPFCYKZ.lKahoLQda0EaAxdqoun1w3DqwNLa2me', '123456789'),
+(2, NULL, 'member@gmail.com', NULL, '$2a$10$j7Upgupou72GBmukz0G6pOATk3wlCAgaoFCEqAhSvLToD/V/1wlpu', NULL),
+(3, NULL, 'shipper@gmail.com', NULL, '$2a$10$u2B29HDxuWVYY3fUJ8R2qunNzXngfxij5GpvlFAEtIz3JpK/WFXF2', NULL),
+(4, 'Ha Noi', 'jvgiveup@gmail.com', 'Pham Tuan', '$2a$10$ZCqCO9gSWt8A8HNXAWq8luqfNbJm0uG3PsUlzry0aRLwO3VHQZTmy', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `san_pham`
+--
+
+CREATE TABLE `san_pham` (
+  `id` bigint(20) NOT NULL,
+  `ma_danh_muc` bigint(20) DEFAULT NULL,
+  `ma_hang_sx` bigint(20) DEFAULT NULL,
+  `ten_san_pham` varchar(255) DEFAULT NULL,
+  `thong_tin_chung` varchar(255) DEFAULT NULL,
+  `don_gia` bigint(20) NOT NULL,
+  `so_luong` int(11) NOT NULL,
+  `thiet_ke` varchar(255) DEFAULT NULL,
+  `mau_sac` varchar(255) NOT NULL,
+  `chat_lieu` varchar(255) NOT NULL,
+  `kich_thuoc` varchar(255) NOT NULL,
+  `thong_tin_bao_hanh` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `san_pham`
+--
+
+INSERT INTO `san_pham` (`id`, `ma_danh_muc`, `ma_hang_sx`, `ten_san_pham`, `thong_tin_chung`, `don_gia`, `so_luong`, `thiet_ke`, `mau_sac`, `chat_lieu`, `kich_thuoc`, `thong_tin_bao_hanh`) VALUES
+(144, 2, 2, 'BÌNH HÚT LỘC VẼ VÀNG THUẬN BUỒM XUÔI GIÓ CAO CẤP H35', '12312123123', 21232, 21, 'nhị cảnh thuận buồm xuôi gió, chữ Lộc', 'xanh lam vẽ vàng', 'Sứ cao cấp vẽ vàng 34k', ' Cao 35 cm', '12 tháng');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `vai_tro`
+--
+
+CREATE TABLE `vai_tro` (
+  `id` bigint(20) NOT NULL,
+  `ten_vai_tro` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `vai_tro`
+--
+
+INSERT INTO `vai_tro` (`id`, `ten_vai_tro`) VALUES
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_MEMBER'),
+(3, 'ROLE_SHIPPER');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `chi_muc_gio_hang`
+--
+ALTER TABLE `chi_muc_gio_hang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK49lmmclnjgb7eck20lwhv0cks` (`ma_gio_hang`),
+  ADD KEY `FKkd69a7wiulr4xgohxl0rlhth4` (`ma_san_pham`);
+
+--
+-- Chỉ mục cho bảng `chi_tiet_don_hang`
+--
+ALTER TABLE `chi_tiet_don_hang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK9wl3houbukbxpixsut6uvojhy` (`ma_don_hang`),
+  ADD KEY `FK3ry84nmdxgoarx53qjxd671tk` (`ma_san_pham`);
+
+--
+-- Chỉ mục cho bảng `danh_muc`
+--
+ALTER TABLE `danh_muc`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `don_hang`
+--
+ALTER TABLE `don_hang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKnwjiboxao1uvw1siemkvs1jb9` (`ma_nguoi_dat`),
+  ADD KEY `FKgndcrlvetoudr3jaif9b7ay37` (`ma_shipper`);
+
+--
+-- Chỉ mục cho bảng `gio_hang`
+--
+ALTER TABLE `gio_hang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKitverect56puwr47y7tyvy6er` (`ma_nguoi_dung`);
+
+--
+-- Chỉ mục cho bảng `hang_san_xuat`
+--
+ALTER TABLE `hang_san_xuat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `lien_he`
+--
+ALTER TABLE `lien_he`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK6jm47uh7f94pc3wix0duvedde` (`ma_nguoi_tra_loi`);
+
+--
+-- Chỉ mục cho bảng `nguoidung_vaitro`
+--
+ALTER TABLE `nguoidung_vaitro`
+  ADD PRIMARY KEY (`ma_nguoi_dung`,`ma_vai_tro`),
+  ADD KEY `FKig6jxd861mqv02a8pn68r43fr` (`ma_vai_tro`);
+
+--
+-- Chỉ mục cho bảng `nguoi_dung`
+--
+ALTER TABLE `nguoi_dung`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `san_pham`
+--
+ALTER TABLE `san_pham`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKqss6n6gtx6lhb7flcka9un18t` (`ma_danh_muc`),
+  ADD KEY `FKchvjvgjnq8lbt9mjtyfn5pksq` (`ma_hang_sx`);
+
+--
+-- Chỉ mục cho bảng `vai_tro`
+--
+ALTER TABLE `vai_tro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `chi_muc_gio_hang`
+--
+ALTER TABLE `chi_muc_gio_hang`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT cho bảng `chi_tiet_don_hang`
+--
+ALTER TABLE `chi_tiet_don_hang`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `danh_muc`
+--
+ALTER TABLE `danh_muc`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT cho bảng `don_hang`
+--
+ALTER TABLE `don_hang`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT cho bảng `gio_hang`
+--
+ALTER TABLE `gio_hang`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT cho bảng `hang_san_xuat`
+--
+ALTER TABLE `hang_san_xuat`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT cho bảng `lien_he`
+--
+ALTER TABLE `lien_he`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `nguoi_dung`
+--
+ALTER TABLE `nguoi_dung`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT cho bảng `san_pham`
+--
+ALTER TABLE `san_pham`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=145;
+
+--
+-- AUTO_INCREMENT cho bảng `vai_tro`
+--
+ALTER TABLE `vai_tro`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `chi_muc_gio_hang`
+--
+ALTER TABLE `chi_muc_gio_hang`
+  ADD CONSTRAINT `FK49lmmclnjgb7eck20lwhv0cks` FOREIGN KEY (`ma_gio_hang`) REFERENCES `gio_hang` (`id`),
+  ADD CONSTRAINT `FKkd69a7wiulr4xgohxl0rlhth4` FOREIGN KEY (`ma_san_pham`) REFERENCES `san_pham` (`id`);
+
+--
+-- Các ràng buộc cho bảng `chi_tiet_don_hang`
+--
+ALTER TABLE `chi_tiet_don_hang`
+  ADD CONSTRAINT `FK3ry84nmdxgoarx53qjxd671tk` FOREIGN KEY (`ma_san_pham`) REFERENCES `san_pham` (`id`),
+  ADD CONSTRAINT `FK9wl3houbukbxpixsut6uvojhy` FOREIGN KEY (`ma_don_hang`) REFERENCES `don_hang` (`id`);
+
+--
+-- Các ràng buộc cho bảng `don_hang`
+--
+ALTER TABLE `don_hang`
+  ADD CONSTRAINT `FKgndcrlvetoudr3jaif9b7ay37` FOREIGN KEY (`ma_shipper`) REFERENCES `nguoi_dung` (`id`),
+  ADD CONSTRAINT `FKnwjiboxao1uvw1siemkvs1jb9` FOREIGN KEY (`ma_nguoi_dat`) REFERENCES `nguoi_dung` (`id`);
+
+--
+-- Các ràng buộc cho bảng `gio_hang`
+--
+ALTER TABLE `gio_hang`
+  ADD CONSTRAINT `FKitverect56puwr47y7tyvy6er` FOREIGN KEY (`ma_nguoi_dung`) REFERENCES `nguoi_dung` (`id`);
+
+--
+-- Các ràng buộc cho bảng `lien_he`
+--
+ALTER TABLE `lien_he`
+  ADD CONSTRAINT `FK6jm47uh7f94pc3wix0duvedde` FOREIGN KEY (`ma_nguoi_tra_loi`) REFERENCES `nguoi_dung` (`id`);
+
+--
+-- Các ràng buộc cho bảng `nguoidung_vaitro`
+--
+ALTER TABLE `nguoidung_vaitro`
+  ADD CONSTRAINT `FKig6jxd861mqv02a8pn68r43fr` FOREIGN KEY (`ma_vai_tro`) REFERENCES `vai_tro` (`id`),
+  ADD CONSTRAINT `FKocavcnspu1wcvp2w0s4usfgbf` FOREIGN KEY (`ma_nguoi_dung`) REFERENCES `nguoi_dung` (`id`);
+
+--
+-- Các ràng buộc cho bảng `san_pham`
+--
+ALTER TABLE `san_pham`
+  ADD CONSTRAINT `FKchvjvgjnq8lbt9mjtyfn5pksq` FOREIGN KEY (`ma_hang_sx`) REFERENCES `hang_san_xuat` (`id`),
+  ADD CONSTRAINT `FKqss6n6gtx6lhb7flcka9un18t` FOREIGN KEY (`ma_danh_muc`) REFERENCES `danh_muc` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
